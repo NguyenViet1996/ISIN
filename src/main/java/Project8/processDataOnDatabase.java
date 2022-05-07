307d2d150a0eb1b7d7d3daa46b281f7f3901df11
ANH_VIET <Viet>
2022-05-07 22:24:01 +0700

    Create file process data on the project ISINS.

---
+++ src/main/java/Project8/processDataOnDatabase.java
@@ -0,0 +1,62 @@
+package Project8;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class processDataOnDatabase {
+
+    private String url;
+
+    public processDataOnDatabase(String url) {
+        this.url = url;
+//        this.dataAfterProcessing = dataAfterProcessing;
+    }
+
+   public void setDataFromDatabase(String url) {
+         this.url = url;
+   };
+
+   public List<Isin> getDataFromDatabase() {
+       Connection con = null;
+       Statement stmt = null;
+       List<Isin> listIsin = new ArrayList<>();
+       try {
+           con = DriverManager.getConnection(url,"SA","");
+           stmt = con.createStatement();
+           ResultSet rs = stmt.executeQuery("select * from ISINS");
+           while(rs.next()) {
+               String nameIsin = rs.getString("name_isin");
+               int quantity = rs.getInt("quantity");
+               int price = rs.getInt("price");
+               Isin isin = new Isin(nameIsin,quantity,price);
+               listIsin.add(isin);
+           }
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+       return listIsin;
+
+   }
+
+    public void setUpdateDatabase(String url,List<Isin> dataAfterProcessing) throws SQLException {
+       Connection con = null;
+       con = DriverManager.getConnection(url,"SA","");
+       String sql = "update ISINS set quantity = ? WHERE name_isin = ? ";
+       try {
+           for(Isin isin : dataAfterProcessing) {
+               PreparedStatement ps = con.prepareStatement(sql);
+               int quantity = isin.getQuantityIsin();
+               String nameIsin = isin.getNameIsin();
+               ps.setInt(1,quantity);
+               ps.setString(2,nameIsin);
+               int result = ps.executeUpdate();
+               System.out.println(result);
+           }
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+
+    }
+
+}
